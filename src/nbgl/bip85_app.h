ea9f469a94b128ca34f1b5e836d31314716e8523
Aido <aido@users.noreply.github.com>
2025-08-25 18:35:26 +0100

    Fixed copyriight headers

--- src/nbgl/bip85_app.h
+++ src/nbgl/bip85_app.h
@@ -1,4 +1,5 @@
 /*******************************************************************************
+ *   Ledger Seed Tool application
  *   (c) 2016-2025 Ledger SAS
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
